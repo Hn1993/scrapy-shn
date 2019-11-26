d120b2e5335b087e9e02ea466aef4082c91a5649
an <12742426874@qq.com>
2019-11-26 15:17:18 +0800

    爬取前5页的数据

--- shein/spiders/spider.py
+++ shein/spiders/spider.py
@@ -6,7 +6,12 @@ from shein.items import SheinItem
 class TiebaSpider(scrapy.Spider):
     name = 'shein'
     allowed_domains = ["https://www.shein.com"]
-    start_urls = ["https://www.shein.com/Sweaters-Cardigans-c-2216.html?ici=0_WomenHomePage_Marketing-Block-1-6_Banner_1_50001_HZ-4-3_aod-0&scici=homepage_162~~0_Banner_1_0_hotZone_7td5q3o67~~4_3~~real_2216~~ccc_shein_pc_women-homepage_default~~0~~50001&sort=3", ]
+    start_urls = ["https://www.shein.com/Sweaters-Cardigans-c-2216.html?ici=0_WomenHomePage_Marketing-Block-1-6_Banner_1_50001_HZ-4-3_aod-0&scici=homepage_162~~0_Banner_1_0_hotZone_7td5q3o67~~4_3~~real_2216~~ccc_shein_pc_women-homepage_default~~0~~50001&sort=3",
+                  "https://www.shein.com/Sweaters-Cardigans-c-2216.html?ici=0_WomenHomePage_Marketing-Block-1-6_Banner_1_50001_HZ-4-3_aod-0&scici=homepage_162~~0_Banner_1_0_hotZone_7td5q3o67~~4_3~~real_2216~~ccc_shein_pc_women-homepage_default~~0~~50001&sort=3&page=2",
+                  "https://www.shein.com/Sweaters-Cardigans-c-2216.html?ici=0_WomenHomePage_Marketing-Block-1-6_Banner_1_50001_HZ-4-3_aod-0&scici=homepage_162~~0_Banner_1_0_hotZone_7td5q3o67~~4_3~~real_2216~~ccc_shein_pc_women-homepage_default~~0~~50001&sort=3&page=3",
+                  "https://www.shein.com/Sweaters-Cardigans-c-2216.html?ici=0_WomenHomePage_Marketing-Block-1-6_Banner_1_50001_HZ-4-3_aod-0&scici=homepage_162~~0_Banner_1_0_hotZone_7td5q3o67~~4_3~~real_2216~~ccc_shein_pc_women-homepage_default~~0~~50001&sort=3&page=4",
+                  "https://www.shein.com/Sweaters-Cardigans-c-2216.html?ici=0_WomenHomePage_Marketing-Block-1-6_Banner_1_50001_HZ-4-3_aod-0&scici=homepage_162~~0_Banner_1_0_hotZone_7td5q3o67~~4_3~~real_2216~~ccc_shein_pc_women-homepage_default~~0~~50001&sort=3&page=5",
+                  ]
 
 
     default_headers = {
@@ -34,25 +39,33 @@ class TiebaSpider(scrapy.Spider):
             item['name'] = i.xpath('a/@title').extract()
             item['price'] = i.xpath('a/@data-price').extract()
             # item['image_urls'] = i.xpath('div[1]/a/img[2]/@data-src').extract()
-            src = 'http:' + i.xpath('div[1]/a/img[2]/@data-src').extract()[0]
+            src = 'http:' + i.xpath('div[1]/a/img[1]/@data-src').extract()[0]
             item['image_urls'] = [src]
 
-            print('-----------',item['name'])
-            print('-----------',item['price'])
-            print('-----------',item['image_urls'])
-
+            # print('-----------',item['name'])
+            # print('-----------',item['price'])
+            # print('-----------',item['image_urls'])
             yield item
 
-            # / html / body / div[1] / div[1] / div / div[1] / div[2] / div[1] / div[4] / div[1]    第一个布局
-
-            # / html / body / div[1] / div[1] / div / div[1] / div[2] / div[2] / div[4] / div[1]    第二个布局
-
-            # item['name'] = response.xpath(
-            #     '/ html / body / div[1] / div[1] / div / div[1] / div[2] / div / div[4] / div[1] / a / @title').extract()
-            # item['price'] = response.xpath(
-            #     '/ html / body / div[1] / div[1] / div / div[1] / div[2] / div / div[4] / div[1] / a / @data-price').extract()
-            # item['image_urls'] = response.xpath(
-            #     '/html/body/div[1]/div[1]/div/div[1]/div[2]/div/div[4]/div[1]/div[1]/a/img[2]/@data-src').extract()
-            # yield item
+        # / html / body / div[1] / div[1] / div / div[1] / div[3] / ul / li[12] / a
+        # / html / body / div[1] / div[1] / div / div[1] / div[3] / ul / li[12] / a
+        # / html / body / div[1] / div[1] / div / div[1] / div[3] / ul / li[11] / a
 
 
+        # next_page1 = response.xpath('/ html / body / div[1] / div[1] / div / div[1] / div[3] / ul / li[12] / a').extract()
+        # next_page2 = response.xpath('/ html / body / div[1] / div[1] / div / div[1] / div[3] / ul / li[11] / a').extract()
+        # next_page_url1 = response.xpath('/ html / body / div[1] / div[1] / div / div[1] / div[3] / ul / li[12] / a / @href').extract()
+        #
+        # if next_page2:
+        #     page_url = response.xpath('/ html / body / div[1] / div[1] / div / div[1] / div[3] / ul / li[11] / a / @href').extract()
+        #     next_page_url = 'https://www.shein.com' + page_url
+        #     print('aaa', next_page_url)
+        #     next_page = response.urljoin(next_page_url)
+        #     yield scrapy.Request(next_page, callback=self.parse)
+        #
+        # if next_page1 and int(next_page_url1[0][-1]) < 6:
+        #     page_url = response.xpath('/ html / body / div[1] / div[1] / div / div[1] / div[3] / ul / li[12] / a / @href').extract()
+        #     next_page_url = 'https://www.shein.com' + page_url
+        #     print('aaa', next_page_url)
+        #     next_page = response.urljoin(next_page_url)
+        #     yield scrapy.Request(next_page, callback=self.parse)
--- shein/test.py
+++ shein/test.py
@@ -1,15 +1,33 @@
 import requests
 from lxml import html, etree
 
-base_urls = 'https://www.shein.com/Sweaters-Cardigans-c-2216.html?ici=0_WomenHomePage_Marketing-Block-1-6_Banner_1_50001_HZ-4-3_aod-0&scici=homepage_162~~0_Banner_1_0_hotZone_7td5q3o67~~4_3~~real_2216~~ccc_shein_pc_women-homepage_default~~0~~50001&sort=3'
+base_urls = 'https://www.shein.com/Sweaters-Cardigans-c-2216.html?ici=0_WomenHomePage_Marketing-Block-1-6_Banner_1_50001_HZ-4-3_aod-0&scici=homepage_162~~0_Banner_1_0_hotZone_7td5q3o67~~4_3~~real_2216~~ccc_shein_pc_women-homepage_default~~0~~50001&sort=3&page=2'
 response = requests.get(base_urls)
 # print(response.text)
 
 s = etree.HTML(response.text)
-next_page = s.xpath('/ html / body / div[1] / div[1] / div / div[1] / div[2] / div / div[4] / div[1] / a / @title')
+# next_page = s.xpath('/ html / body / div[1] / div[1] / div / div[1] / div[2] / div / div[4] / div[1] / a / @title')
 image_url = s.xpath('/html/body/div[1]/div[1]/div/div[1]/div[2]/div/div[4]/div[1]/div[1]/a/img[2]/@data-src')
+next_page = s.xpath('/html/body/div[1]/div[1]/div/div[1]/div[3]/ul/li/a/text()')
+
+next_page = s.xpath('/ html / body / div[1] / div[1] / div / div[1] / div[3] / ul / li[12] / a')
+next_page_url = s.xpath('/ html / body / div[1] / div[1] / div / div[1] / div[3] / ul / li[12] / a / @href')
+print(next_page)
+print(next_page_url)
+print(next_page_url[0][-1])
+
+# if next_page:
+
+    # next_page = s.urljoin(next_page_url)
+
+
+
+# /html/body/div[1]/div[1]/div/div[1]/div[3]/ul/li[11]/a
+# /html/body/div[1]/div[1]/div/div[1]/div[3]/ul/li[12]/a
+
 # /html/body/div[1]/div[1]/div/div[1]/div[2]/div[4]/div[4]/div[1]/div[1]/a/img[2]
 #
 # /html/body/div[1]/div[1]/div/div[1]/div[2]/div[7]/div[4]/div[1]/div[1]/a/img[2]
-print(image_url)
-print(len(image_url))
+# print(image_url)
+# print(next_page)
+# print(len(image_url))
